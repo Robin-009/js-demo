c665bb70d3662f1107b7e794f31424621b0e6e5b
Robin <138255602+Robin-009@users.noreply.github.com>
2024-05-16 14:06:20 +0000

    strings javascript

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+// "hello" + "world" => concatenation
+
+const repoCount = 50
+
+//console.log(`hello my name is ${name} and my age is ${repoCount}.`)
+
+const gameName = new String('adarsh')
+console.log(gameName[0])
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(5))
+console.log(gameName.indexOf('s'))
+
+const str = gameName.substring(0,4)
+console.log(str); // => adar
+
+const str1 = gameName.slice(-5.3)
+console.log(str1);// => darsh
