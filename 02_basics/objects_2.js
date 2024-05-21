1aef2b22163dd730988c78fc4037215fbaaf862d
Robin <138255602+Robin-009@users.noreply.github.com>
2024-05-21 21:28:13 +0000

    object part 2 destructure , javascript object notion

---
+++ 02_basics/objects_2.js
@@ -0,0 +1,22 @@
+const course = {
+    course_name : "cse",
+    price : "100000",
+    instructor : "vipin"
+}
+
+const {instructor : faculty} = course; // => destructure
+
+console.log(faculty);
+
+// const navbar = ({company})} => {
+
+
+// }
+
+// navbar{caompany = "bruce"}   => destructure in react
+
+// {
+//     name : "robin",
+//     desig : "student",
+//     salary : 0
+// }
