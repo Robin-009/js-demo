b3d579740933c5c110b67196fd4371d57cb208f4
Robin <138255602+Robin-009@users.noreply.github.com>
2024-07-19 06:36:47 +0000

    if-else and swichcase staements

---
+++ 04_control_flow/one.js
@@ -0,0 +1,27 @@
+ // #if
+const isUserLoggedIn =true;
+
+if(2 === "2"){
+    console.log("hahaha")
+}else{
+    console.log("nikal");
+}
+//  if(true){
+//     // executed
+//  }
+
+//  if(flase){
+//     not executed ;
+//  }
+
+ //   " === " it checks for same value and type.
+ //  " == " it only checks for same value.
+
+
+ // Short Hand Notation :
+  
+ // if(balance > 500) console.log("test");  //=> implicit scope
+
+//  const balance =1000;
+
+// -> Swich Case Statements 
